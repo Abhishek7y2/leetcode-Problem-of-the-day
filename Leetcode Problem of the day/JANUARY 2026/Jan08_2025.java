f04c0a261854462e8f9dbff5c49bd2adfde917da
Abhishek7y2 <224711228+Abhishek7y2@users.noreply.github.com>
2026-01-09 00:20:14 +0530

    Add files via upload

---
+++ Leetcode Problem of the day/JANUARY 2026/Jan08_2025.java
@@ -0,0 +1,54 @@
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int max = Integer.MIN_VALUE;
+        int ans = 1;
+        int curr = 1;
+
+        while (!q.isEmpty()) {
+            int sum = 0;
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                sum += temp.val;
+
+                if (temp.left != null)
+                    q.add(temp.left);
+
+                if (temp.right != null)
+                    q.add(temp.right);
+            }
+
+            if (sum > max) {
+                max = sum;
+                ans = curr;
+            }
+
+            curr++;
+        }
+
+        return ans;
+    }
+}
